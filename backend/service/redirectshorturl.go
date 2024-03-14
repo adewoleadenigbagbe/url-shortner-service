810dd0bdc01757ec4e6c4ad9940dc5aae190a0e0
Adewole <wolecharles@gmail.com>
2024-03-14 12:30:07 +0100

    add hit count to redirect endpoint

--- backend/service/redirectshorturl.go
+++ backend/service/redirectshorturl.go
@@ -21,14 +21,31 @@ func (service UrlService) RedirectShort(urlContext echo.Context) error {
 		return urlContext.JSON(http.StatusBadRequest, errors.New("shortlink is invalid"))
 	}
 
-	var short string
-	query := `SELECT shortlinks.OriginalUrl, domains.Name FROM shortlinks JOIN domains ON shortlinks.DomainId = domains.Id 
+	var originalUrl string
+	var hits int64
+	var id string
+	query := `SELECT shortlinks.Id,shortlinks.OriginalUrl, shortlinks.Hits FROM shortlinks JOIN domains ON shortlinks.DomainId = domains.Id 
 	 WHERE shortlinks.Hash=? AND shortlinks.IsDeprecated=? AND domains.IsDeprecated=?`
 	row := service.Db.QueryRow(query, request.ShortUrl, false, false)
-	if err = row.Scan(&short); errors.Is(err, sql.ErrNoRows) {
+	if err = row.Scan(&id, &originalUrl, &hits); errors.Is(err, sql.ErrNoRows) {
 		return urlContext.JSON(http.StatusNotFound, err.Error())
 	}
 
-	urlContext.Response().Header().Set("Location", short)
+	//access the logs information
+	tx, err := service.Db.Begin()
+	if err != nil {
+		return urlContext.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	_, err = tx.Exec("UPDATE shortlinks SET Hits =? WHERE Id =?", hits+1, id)
+	if err != nil {
+		tx.Rollback()
+		return urlContext.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	//create accesslogs
+	tx.Commit()
+
+	urlContext.Response().Header().Set("Location", originalUrl)
 	return urlContext.JSON(http.StatusFound, nil)
 }
